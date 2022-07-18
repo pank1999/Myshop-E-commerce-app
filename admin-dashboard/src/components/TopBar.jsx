fbbd65413cdedab4abab91073c7eb2f7f7551f2a
Your Name <pankajpandey9589@gmail.com>
2022-07-18 13:31:50 +0530

    completed TopBar of Admin Dashboard

--- admin-dashboard/src/components/TopBar.jsx
+++ admin-dashboard/src/components/TopBar.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import "./TopBar.css";
+import {NotificationsNone , Language ,Settings} from '@material-ui/icons';
 
 export default function TopBar() {
   return (
-    <div>
-       topBar
+    <div className='topBar'>
+       <div className='topBarWrapper'>
+           <div className='topLeft'>
+               <span className='logo'>MyShopAdmin</span>
+           </div>
+           <div className='topRight'>
+                <div className='topBarIcons'>
+                     <NotificationsNone />
+                     <span className='topIconBag'>2</span>
+                </div>
+                <div className='topBarIcons'>
+                     <Language />
+                </div>
+                <div className='topBarIcons'>
+                     <Settings/>
+                     
+                </div>
+                <img src='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80' alt='' className='topAvatar'/>
+
+                
+           </div>    
+       </div>
+
     </div>
   )
 }
