36405b6cadbddf9dc15daf96d5b5f9c6407066fb
Your Name <pankajpandey9589@gmail.com>
2022-07-18 17:48:48 +0530

    completd sideBar of the Dashboard

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -1,9 +1,18 @@
+import SideBar from "./components/SideBar/SideBar";
 import TopBar from "./components/TopBar";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
         <TopBar/>
+        <div className="container">
+          <SideBar />
+          <div className="others">
+             other pages
+          </div>
+        </div>
+        
         
 
     </div>
---
+++ admin-dashboard/src/components/SideBar/SideBar.jsx
@@ -0,0 +1,97 @@
+import './Sidebar.css';
+import {ChatBubbleOutline, FeedbackOutlined, LineStyle, MailOutline, PersonOutline, ReportOutlined, Timeline, TrendingUp, AccountCircleOutlined} from '@material-ui/icons';
+export default function SideBar() {
+  return (
+    <div className="sidebar">
+        <div className='sideBarWrapper'>
+         <div className='sideBarMenu'>
+            <h3 className='sideBarTitle'>
+                    Dashboard
+            </h3>
+            <ul className='sideBarList'>
+                 <li className='sideBarListItem active'>
+                    <LineStyle className='sideBarIcon'/>
+                    Home
+                 </li>
+                 <li className='sideBarListItem'>
+                    <Timeline className='sideBarIcon'/>
+                    Analytics
+                 </li>
+                 <li className='sideBarListItem'>
+                    <TrendingUp className='sideBarIcon'/>
+                    Sales
+                 </li>
+                 
+            </ul>
+        </div>
+    </div>
+
+    <div className='sideBarMenu'>
+            <h3 className='sideBarTitle'>
+                    Quick Menu
+            </h3>
+            <ul className='sideBarList'>
+                 <li className='sideBarListItem'>
+                    <PersonOutline className='sideBarIcon'/>
+                    User
+                 </li>
+                 <li className='sideBarListItem'>
+                    <Timeline  className='sideBarIcon'/>
+                    Products
+                 </li>
+                 <li className='sideBarListItem'>
+                    <TrendingUp  className='sideBarIcon'/>
+                    Transactions
+                 </li>
+                 <li className='sideBarListItem'>
+                    <ReportOutlined  className='sideBarIcon'/>
+                    Report
+                 </li>
+            </ul>
+        </div>
+
+        <div className='sideBarMenu'>
+            <h3 className='sideBarTitle'>
+                    Notifications
+            </h3>
+            <ul className='sideBarList'>
+                 <li className='sideBarListItem'>
+                    <MailOutline className='sideBarIcon'/>
+                    Mail
+                 </li>
+                 <li className='sideBarListItem'>
+                    <FeedbackOutlined  className='sideBarIcon'/>
+                    Feedback
+                 </li>
+                 <li className='sideBarListItem'>
+                    <ChatBubbleOutline  className='sideBarIcon'/>
+                    Messages
+                 </li>
+                 
+            </ul>
+        </div>
+    
+        <div className='sideBarMenu'>
+            <h3 className='sideBarTitle'>
+                    Stuff
+            </h3>
+            <ul className='sideBarList'>
+                 <li className='sideBarListItem'>
+                    <AccountCircleOutlined className='sideBarIcon'/>
+                    Manage
+                 </li>
+                 <li className='sideBarListItem'>
+                    <Timeline  className='sideBarIcon'/>
+                    Feedback
+                 </li>
+                 <li className='sideBarListItem'>
+                    <TrendingUp  className='sideBarIcon'/>
+                    Reports
+                 </li>
+                 
+            </ul>
+        </div>
+    
+    </div>
+  )
+}
